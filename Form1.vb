f84fb5e3f19f0e657b9246df66fc3255f6db4eea
OmarMazin <77233657+omarmazin@users.noreply.github.com>
2021-09-05 23:08:41 +0300

    Delete Form1.vb

--- Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Net, System.Text.RegularExpressions
-Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim web As New WebClient
-        Dim res As String = web.DownloadString("http://localhost/Prog%20manager/api.php")
-        Dim reg As String = Regex.Match(res, My.Resources.String1).Groups(1).Value
-        Label1.Text = reg
-    End Sub
-End Class
