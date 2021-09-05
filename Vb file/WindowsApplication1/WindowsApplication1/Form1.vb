cf71db5fcc1e35ddc52efbeee0e75255d3b2af20
OmarMazin <77233657+omarmazin@users.noreply.github.com>
2021-09-05 20:28:24 +0000

    Add files via upload

---
+++ Vb file/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Net, System.Text.RegularExpressions
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim web As New WebClient
+        Dim res As String = web.DownloadString("http://localhost/prog%20manager/api.php").ToString
+        Dim reg As String = Regex.Match(res, My.Resources.String1).Groups(1).Value
+        Label1.Text = reg
+    End Sub
+End Class
